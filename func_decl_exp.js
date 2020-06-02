f1091880214e453dc0a79a2ebfa41cf30c2fbd26
VladaGaydey <54859369+VladaGaydey@users.noreply.github.com>
2020-06-02 18:51:51 +0300

    Add files via upload

---
+++ func_decl_exp.js
@@ -0,0 +1,66 @@
+"use strict";
+function l1(str, find_sym) {
+
+    find_sym = new Set(find_sym);
+    let counter = 0;
+    [...str].forEach(el => counter += find_sym.has(el)?1:0); // l1("asds", "as"); // lab arrays begin
+    return counter; 
+}
+
+function l2() {
+    function getRandom(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+      let arr = [];
+    
+    for (let i = 0;i < 20; i++){
+        arr.push(getRandom(0, 100));
+    }
+    
+    arr.forEach(el => console.log(el));
+}
+
+let global_var = 0;
+
+function func_declar(param1, _param2, _param3, _parampam = "pam") {
+    console.log("this is  Function Declaration");
+
+    let local_var = global_var;
+    local_var++; // using global var as lorem ipsum
+
+    if (param1){
+        console.log("lorem ipsum");
+    }else{
+        console.log("missing argument param1");
+    }
+    
+
+}
+
+let func_exp = function (param1, _param2, _param3, parampam = "pam") {
+    console.log(param1);
+    console.log(parampam);
+    console.log("this is function expression");
+
+    global_var++ // changing global var inside as last pidor :)
+
+};
+
+
+
+console.log(global_var);
+func_exp();
+func_declar();
+console.log(global_var);
+
+
+// Lab server
+
+const express = require("express");
+const app = express();
+
+app.get("/*", function(_request, response){
+     
+    response.send(_request.url + " 80");
+});
+app.listen(80);
